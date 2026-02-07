2831685c9e6cd58afe4c447027b067d7b9eef8c3
Danik499 <danikshcherban499@gmail.com>
2026-02-07 21:22:41 +0200

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestIcons = [
+  {
+    src: "apple-app-store.png",
+    sizes: "512x512",
+    type: "image/png",
+  },
+];
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "inline",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        icons: manifestIcons,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
+});
